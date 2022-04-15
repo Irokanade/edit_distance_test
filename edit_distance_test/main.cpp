76931e7ff0164972d06c9181524d318edbe4258e
irokanade <michaeljs.leong@gmail.com>
2022-04-15 15:53:15 +0800

    init commit

--- edit_distance_test/main.cpp
+++ edit_distance_test/main.cpp
@@ -6,9 +6,44 @@
 //
 
 #include <iostream>
+#include <cstring>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    char oriString[1024] = {0};
+    char newString[1024] = {0};
+    
+    cin >> oriString;
+    cin >> newString;
+    
+    int oriStringLen = (int)strlen(oriString);
+    int newStringLen = (int)strlen(newString);
+    
+    //dyanamic programming approach
+    int d[newStringLen+1][oriStringLen+1]; //tablular approach
+    
+    //fill table
+    for(int i = 0; i <= newStringLen; i++) {
+        for(int j = 0; j <= oriStringLen; j++) {
+            if(j == 0) {
+                d[i][j] = i;
+            } else if(i == 0) {
+                d[i][j] = j;
+            } else {
+                d[i][j] = std::min({d[i-1][j-1]+(oriString[j-1]!=newString[i-1]), d[i-1][j]+1, d[i][j-1]+1}); //min
+            }
+        }
+    }
+    
+    //print table
+    for(int i = 0; i <= newStringLen; i++) {
+        for(int j = 0; j <= oriStringLen; j++) {
+            cout << d[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+    
+    cout << "least amount of operations: " << d[newStringLen][oriStringLen] << '\n';
+    
     return 0;
 }
